a9cc5d03fa7cc8e8078643bc1bfb908c5a393882
Volha Valasevich <volha.valasevich@n1affiliates.com>
2023-10-02 16:03:00 +0200

    fix: config fixes

--- app.tsx
+++ app.tsx
@@ -1,5 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import jwt_decode from 'jwt-decode';
+import SignUpPage from './src/pages/SignUpPage/SignUpPage.tsx';
+
 const App = (): JSX.Element => {
-  return <div>App default config</div>
-}
-export default App
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id:
+        '983865956836-ehqa2t8fnjgen55eberv9ugv57qunej7.apps.googleusercontent.com',
+      // callback: (response) => console.log(jwt_decode(response.credential)),
+      callback: (response) => console.log(response),
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById('google-signin-button'),
+      { theme: 'outline', size: 'large' }
+    );
+  }, []);
+
+  return (
+    <div>
+     <div id='google-signin-button'></div>
+    </div>
+  );
+};
+export default App;
