10f6fdd6870ce7b4be70debfb8d0e3d5d7098d65
Volha Valasevich <volha.valasevich@n1affiliates.com>
2023-10-18 21:45:00 +0200

    feat: encrypt & send users email

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
+import { encrypt } from './services';
+import usersApiClient from './../../api/UsersClient/UsersClient';
 
 const Auth = (): JSX.Element => {
+  const [userEmail, setUserEmail] = useState<string>('');
   useEffect(() => {
     google.accounts.id.initialize({
       client_id:
         '983865956836-ehqa2t8fnjgen55eberv9ugv57qunej7.apps.googleusercontent.com',
-      callback: (response) => { console.log(jwt_decode(response.credential)); },
+      callback: (response) => {
+        setUserEmail(jwt_decode(response.credential).email);
+        console.log(jwt_decode(response.credential));
+      },
       auto_select: true,
     });
 
@@ -17,11 +23,24 @@ const Auth = (): JSX.Element => {
     );
     google.accounts.id.prompt();
   }, []);
+  useEffect(() => {
+    if (userEmail.length > 0) {
+      const encryptedEmail = encrypt(userEmail);
+      console.log(encryptedEmail, 'encryptedEmail');
+      usersApiClient.registerUser(encryptedEmail)
+        .then((res) => {
+          console.log(res, 'works');
+        })
+        .catch((err) => {
+          console.log(err, 'err');
+        });
+    }
+  }, [userEmail]);
   return (
     <div>
-     <div id='google-signin-button'></div>
-     <Link to='/sign-up'>Sign Up</Link>
-     <Link to='/'>Home</Link>
+      <div id='google-signin-button'></div>
+      <Link to='/sign-up'>Sign Up</Link>
+      <Link to='/'>Home</Link>
     </div>
   );
 };
